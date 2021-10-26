2a8d6e28a8f66734e9e4268028120313356fec06
Klay <techwklay@gmail.com>
2021-10-26 01:05:50 -0400

    Navbar finished

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,16 +1,25 @@
 import React from "react";
 import "./topbar.scss";
+import { Code } from "@material-ui/icons";
 
-export default function Topbar() {
+export default function Topbar({ menuOpen, setMenuOpen }) {
   return (
-    <div className="topbar" id="topbar">
+    <div className={"topbar " + (menuOpen && "active")} id="topbar">
       <div className="wrapper">
         <div className="left">
+          <Code style={{ fontSize: 50 }} />
+
           <a href="#intro" className="logo">
             Klay Ahmed
           </a>
         </div>
-        <div className="right">menu</div>
+        <div className="right">
+          <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
       </div>
     </div>
   );
