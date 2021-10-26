44c3e26d0f8a068e15c2cd35db86b2e61b6a40a5
Klay <techwklay@gmail.com>
2021-10-25 20:19:50 -0400

    navbar and colors

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import "./topbar.scss";
 
 export default function Topbar() {
-  return <div className="topbar">
-            <div className="wrapper">
-              <div className="left"></div>
-              <div className="right"></div>
-            </div>
-  
-  
-         </div>;
+  return (
+    <div className="topbar" id="topbar">
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">
+            Klay Ahmed
+          </a>
+        </div>
+        <div className="right">menu</div>
+      </div>
+    </div>
+  );
 }
